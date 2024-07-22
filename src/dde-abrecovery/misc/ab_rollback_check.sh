9c4df13ea88f374ab72abfaec09978c99784185c
lichangze <lichangze@uniontech.com>
2024-07-22 09:44:43 +0800

    fix: 适配deepin-ab-recovery dbus安全规范整改
    备份标记文件由/.deepin-ab-recovery-backup改为/usr/share/deepin-ab-recovery/.deepin-ab-recovery-backup
    
    Log: 适配deepin-ab-recovery dbus安全规范整改
    Bug: https://pms.uniontech.com/bug-view-265227.html
    Change-Id: I7b26f6506ff9da137449cfd3b6da35d20165e69e

--- src/dde-abrecovery/misc/ab_rollback_check.sh
+++ src/dde-abrecovery/misc/ab_rollback_check.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-ab_recovery_mark_file="/.deepin-ab-recovery-backup"
-if [ ! -f $ab_recovery_mark_file ]; then
-  echo "$ab_recovery_mark_file does not exist, don't need process rollback"
+ab_recovery_mark_file="/usr/share/deepin-ab-recovery/.deepin-ab-recovery-backup"
+ab_recovery_mark_file_old="/.deepin-ab-recovery-backup"
+if [[ ! -e $ab_recovery_mark_file && ! -e $ab_recovery_mark_file_old ]]; then
+  echo "$ab_recovery_mark_file and $ab_recovery_mark_file_old not exist, don't need process rollback"
   exit 1
 fi
+
 
 can_restore=$(qdbus --literal --system com.deepin.ABRecovery /com/deepin/ABRecovery com.deepin.ABRecovery.CanRestore)
 if [ "$can_restore" = "false" ];then
