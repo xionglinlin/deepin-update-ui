a2a8ef3a5d1a597383401d3a947c685846cd0547
zhangkun <zhangkun2@uniontech.com>
2025-04-21 15:12:25 +0800

    chore(license): Organizing licenses and copyrights
    as title
    
    pms: TASK-368711

--- src/dde-update/abstractfullbackgroundinterface.h
+++ src/dde-update/abstractfullbackgroundinterface.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef ABSTRACTFULLBACKGROUND_H
 #define ABSTRACTFULLBACKGROUND_H
 
